a091cdc6892690ed7319ffce7b50c7e38b22973c
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-28 14:55:05 +0200

    feat: initial commit

---
+++ remir/src/lib.rs
@@ -0,0 +1,3 @@
+//! Remir is an efficient and capable Middle Intermediate Representation for Calscin and other programming languages
+//!
+//! It uses a block-like representation with instructions and SSA values.
