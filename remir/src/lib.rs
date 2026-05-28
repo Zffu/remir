7f3a373c4f64caf0a27252100e94546f2902d242
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-28 15:01:37 +0200

    feat: added BaseSSAValue

--- remir/src/lib.rs
+++ remir/src/lib.rs
@@ -1,3 +1,5 @@
-//! Remir is an efficient and capable Middle Intermediate Representation for Calscin and other programming languages
+//! Remir is an efficient, capable, and safe Middle Intermediate Representation for Calscin and other programming languages
 //!
 //! It uses a block-like representation with instructions and SSA values.
+
+pub mod values;
---
+++ remir/src/values/mod.rs
@@ -0,0 +1,26 @@
+//! Definitions for values in the Remir MIR
+
+/// The type of the SSA MIR values.
+pub enum ValueType {
+    Float,
+    Int,
+    Pointer,
+}
+
+pub struct BaseSSAValue {
+    /// The index responsible to the creation of said value
+    pub inst_ind: usize,
+
+    /// The type of value
+    pub value_type: ValueType,
+}
+
+impl BaseSSAValue {
+    /// Creates a new [`BaseSSAValue`] based on the given type and the given instruction index
+    pub fn new(inst_ind: usize, value_type: ValueType) -> Self {
+        Self {
+            inst_ind,
+            value_type,
+        }
+    }
+}
