4275f2a1f7b8e0d0e344aa2bd5f40fe4e2f64490
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-29 18:03:11 +0200

    feat: added block

--- remir/src/block/mod.rs
+++ remir/src/block/mod.rs
@@ -5,3 +5,21 @@ pub struct BlockReference {
     pub name: String,
     pub id: usize,
 }
+
+/// Represents a function block / branch.
+pub struct Block {
+    /// The inner reference to feed.
+    pub reference: BlockReference,
+}
+
+impl BlockReference {
+    pub fn new(name: String, id: usize) -> Self {
+        Self { name, id }
+    }
+}
+
+impl Block {
+    pub fn new(reference: BlockReference) -> Self {
+        Self { reference }
+    }
+}
