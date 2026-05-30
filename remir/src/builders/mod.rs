ad33a811859216d720b0a0bdd3984548d405485b
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-30 16:13:50 +0200

    feat: added copy and load builders

--- remir/src/builders/mod.rs
+++ remir/src/builders/mod.rs
@@ -1,5 +1,7 @@
 //! Functions used to build instructions and directly append them into the module.
 
 pub mod consts;
+pub mod regs;
 
 pub use consts::*;
+pub use regs::*;
---
+++ remir/src/builders/regs.rs
@@ -0,0 +1,18 @@
+use crate::{
+    insts::Instruction,
+    module::Module,
+    values::{BaseSSAValue, ptr::SSAPointerValue},
+    writer::InstructionWriter,
+};
+
+pub fn build_copy(module: &mut Module, val: BaseSSAValue) -> Result<BaseSSAValue, ()> {
+    let inst = Instruction::Copy { val };
+
+    module.write(inst).get()
+}
+
+pub fn build_load(module: &mut Module, source: SSAPointerValue) -> Result<BaseSSAValue, ()> {
+    let inst = Instruction::Load { source };
+
+    module.write(inst).get()
+}
