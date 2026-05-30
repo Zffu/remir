c20d3c93d46f03d2a99484dd0d811614ab4add24
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-30 16:31:55 +0200

    feat: added math builders

---
+++ remir/src/builders/math.rs
@@ -0,0 +1,65 @@
+use crate::{
+    insts::Instruction,
+    module::Module,
+    utils::operators::MathOperator,
+    values::{float::SSAFloatValue, int::SSAIntValue},
+    writer::InstructionWriter,
+};
+
+pub fn build_math_op_int(
+    module: &mut Module,
+    a: SSAIntValue,
+    b: SSAIntValue,
+    op: MathOperator,
+    signed: bool,
+    signed_wrap: bool,
+    unsigned_wrap: bool,
+    fast: bool,
+) -> Result<SSAIntValue, ()> {
+    if (a.signed != b.signed && !signed) || (a.signed && !signed) {
+        return Err(()); // Enforces sign	
+    }
+
+    let inst = Instruction::MathOperationInt {
+        a,
+        b,
+        op,
+        signed,
+        signed_wrap,
+        unsigned_wrap,
+        fast,
+    };
+
+    let val = module.write(inst).get()?;
+
+    val.try_into()
+}
+
+pub fn build_math_op_float(
+    module: &mut Module,
+    a: SSAFloatValue,
+    b: SSAFloatValue,
+    op: MathOperator,
+    signed: bool,
+    signed_wrap: bool,
+    unsigned_wrap: bool,
+    fast: bool,
+) -> Result<SSAFloatValue, ()> {
+    if (a.signed != b.signed && !signed) || (a.signed && !signed) {
+        return Err(()); // Enforces sign	
+    }
+
+    let inst = Instruction::MathOperationFloat {
+        a,
+        b,
+        op,
+        signed,
+        signed_wrap,
+        unsigned_wrap,
+        fast,
+    };
+
+    let val = module.write(inst).get()?;
+
+    val.try_into()
+}
--- remir/src/builders/mod.rs
+++ remir/src/builders/mod.rs
@@ -1,7 +1,9 @@
 //! Functions used to build instructions and directly append them into the module.
 
 pub mod consts;
+pub mod math;
 pub mod regs;
 
 pub use consts::*;
+pub use math::*;
 pub use regs::*;
