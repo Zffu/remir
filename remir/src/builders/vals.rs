f8ef95a1c51f848f2d9144cf2d30c5658585172b
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-31 14:39:24 +0200

    feat: added int & float modification builders

--- remir/src/builders/vals.rs
+++ remir/src/builders/vals.rs
@@ -1,7 +1,7 @@
 use crate::{
     insts::Instruction,
     module::Module,
-    values::{BaseSSAValue, ValueType, int::SSAIntValue},
+    values::{BaseSSAValue, ValueType, float::SSAFloatValue, int::SSAIntValue},
     writer::InstructionWriter,
 };
 
@@ -34,4 +34,116 @@ pub fn build_select(
     };
 
     module.write(inst).get()
+}
+
+pub fn int_to_float(
+    module: &mut Module,
+    val: SSAIntValue,
+    into: ValueType,
+) -> Result<SSAFloatValue, ()> {
+    if let ValueType::Float(_, _) = into {
+        let inst = Instruction::IntToFloat { val, into };
+
+        let val = module.write(inst).get()?;
+
+        val.try_into()
+    } else {
+        Err(()) // Target type is not a float
+    }
+}
+
+pub fn float_to_int(
+    module: &mut Module,
+    val: SSAFloatValue,
+    into: ValueType,
+) -> Result<SSAIntValue, ()> {
+    if let ValueType::Int(_, _) = into {
+        let inst = Instruction::FloatToInt { val, into };
+
+        let val = module.write(inst).get()?;
+
+        val.try_into()
+    } else {
+        Err(()) // Target type is not an int
+    }
+}
+
+pub fn build_int_extend(
+    module: &mut Module,
+    val: SSAIntValue,
+    into: ValueType,
+) -> Result<SSAIntValue, ()> {
+    if let ValueType::Int(_, size) = &into {
+        if val.size >= *size {
+            return Err(()); // Use int truncate instead
+        }
+
+        let inst = Instruction::IntExtend { val, into };
+
+        let val = module.write(inst).get()?;
+
+        val.try_into()
+    } else {
+        Err(()) // Target type is not an int
+    }
+}
+
+pub fn build_int_truncate(
+    module: &mut Module,
+    val: SSAIntValue,
+    into: ValueType,
+) -> Result<SSAIntValue, ()> {
+    if let ValueType::Int(_, size) = &into {
+        if val.size <= *size {
+            return Err(()); // Use int extend instead
+        }
+
+        let inst = Instruction::IntTruncate { val, into };
+
+        let val = module.write(inst).get()?;
+
+        val.try_into()
+    } else {
+        Err(()) // Target type is not an int
+    }
+}
+
+pub fn build_float_extend(
+    module: &mut Module,
+    val: SSAFloatValue,
+    into: ValueType,
+) -> Result<SSAFloatValue, ()> {
+    if let ValueType::Float(_, size) = &into {
+        if val.size >= *size {
+            return Err(()); // Use float truncate instead
+        }
+
+        let inst = Instruction::FloatExtend { val, into };
+
+        let val = module.write(inst).get()?;
+
+        val.try_into()
+    } else {
+        Err(()) // Target type is not a float
+    }
+}
+
+pub fn build_float_truncate(
+    module: &mut Module,
+    val: SSAFloatValue,
+    into: ValueType,
+) -> Result<SSAFloatValue, ()> {
+    if let ValueType::Float(_, size) = &into {
+        if val.size <= *size {
+            return Err(()); // Use float extend instead
+        }
+
+        let inst = Instruction::FloatTruncate { val, into };
+
+        let val = module.write(inst).get()?;
+
+        val.try_into()
+    } else {
+        Err(()) // Target type is not a float
+    }
 }
