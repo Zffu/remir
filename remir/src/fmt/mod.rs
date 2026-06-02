a3767a82a5b71af73ee463886d1ee338f3a89a44
Zffu <103074097+Zffu@users.noreply.github.com>
2026-06-02 11:44:35 +0200

    feat: added fmt for values

--- remir/src/fmt/mod.rs
+++ remir/src/fmt/mod.rs
@@ -1,1 +1,3 @@
 //! Formatting module for every element inside of Remir. Allows Remir to generate IR files
+
+pub mod vals;
---
+++ remir/src/fmt/vals.rs
@@ -0,0 +1,50 @@
+use std::fmt::Display;
+
+use crate::values::{
+    BaseSSAValue, SSAValueLike, consts::ConstantData, float::SSAFloatValue, int::SSAIntValue,
+    ptr::SSAPointerValue, structs::SSAStructValue,
+};
+
+impl Display for ConstantData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let res = match self {
+            Self::None => "??",
+            Self::Float(val) => &format!("{}", val),
+            Self::Int(val) => &format!("{}", val),
+            Self::Pointer(val) => &format!("*{}", val),
+        };
+
+        write!(f, "{}", res)
+    }
+}
+
+macro_rules! decl_value_display {
+    ($type: ty) => {
+        impl Display for $type {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                let const_data = self.get_constant_data();
+                let display_const_data = const_data.is_something();
+
+                write!(f, "{} ", self.get_type());
+
+                if display_const_data {
+                    write!(f, "{} (", const_data);
+                }
+
+                write!(f, "#{}", self.get_inst_index());
+
+                if display_const_data {
+                    write!(f, ")");
+                }
+
+                Ok(())
+            }
+        }
+    };
+}
+
+decl_value_display!(BaseSSAValue);
+decl_value_display!(SSAIntValue);
+decl_value_display!(SSAFloatValue);
+decl_value_display!(SSAPointerValue);
+decl_value_display!(SSAStructValue);
