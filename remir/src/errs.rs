410cbeab86a6d53309b0cc64aa1fe549bc41526f
Zffu <103074097+Zffu@users.noreply.github.com>
2026-06-04 09:34:13 +0200

    feat: added return_err macro

--- remir/src/errs.rs
+++ remir/src/errs.rs
@@ -1,11 +1,36 @@
 #[cfg(feature = "errors")]
+pub type RemirResult<K> = Result<K, RemirError>;
+
+#[cfg(not(feature = "errors"))]
+pub type RemirResult<K> = Result<K, ()>;
+
+#[cfg(feature = "errors")]
 #[derive(Debug)]
 pub struct RemirError {
     pub msg: String,
 }
 
 #[cfg(feature = "errors")]
-pub type RemirResult<K> = Result<K, RemirError>;
+impl RemirError {
+    pub fn new(str: &str) -> Self {
+        Self {
+            msg: str.to_string(),
+        }
+    }
+}
+
+#[cfg(feature = "errors")]
+#[macro_export]
+macro_rules! return_err {
+    ($msg: literal) => {
+        return Err(RemirError::new($msg))
+    };
+}
 
 #[cfg(not(feature = "errors"))]
-pub type RemirResult<K> = Result<K, ()>;
+#[macro_export]
+macro_rules! return_err {
+    ($msg: literal) => {
+        return Err(())
+    };
+}
